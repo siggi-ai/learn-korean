f76c0a2d57a5a6036d51b8e5fa7ef572e0a31269
siggi <siegfried.hamm@gmail.com>
2022-06-01 10:01:57 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Bungee+Hairline&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap"
+      rel="stylesheet"
+    />
+    <div class="top-box">
+      <div class="top-content">
+        <br />
+        <br />
+        <router-link to="/" class="menu-bar">home</router-link>
+        <!-- <router-link to="/a1" class="menu-bar">a1</router-link> -->
+        <br />
+        <br />
+        <br />
+      </div>
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  data: function () {
+    return {
+      localTime: " ",
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.menu-bar {
+    text-align: center;
+    margin-left: 0 auto;
+    margin-top: 37px;
+    padding: 16px;
+    font-size: 2rem;
+    margin: 0 auto;
+    width: 500px;
+    font-family: 'Koulen';
+    color: #ffc800;
+    text-decoration: none;
+}
+
+a {
+  color: rgb(75, 5, 237);
+  text-decoration: wavy;
+}
+
+body {
+  margin: 0 auto;
+}
+
+.top-box {
+  margin: 0 auto;
+  width: 586px;
+}
+
+.top-content {
+  margin: 0 auto;
+  width: 690px;
+}
+
+@media screen and (max-width: 800px) {
+  .menu-bar {
+    margin-left: 50px;
+  }
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,328 @@
+<template>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Koulen&family=Macondo&display=swap"
+    rel="stylesheet"
+  />
+  <div>
+    <div class="container">
+      
+      <p class="text">Music-Website</p>
+      <p class="text2">Explore Sounds</p>
+      <p class="text3">Experience Emotions</p>
+    </div>
+
+    <div class="scroll-container">
+      <div class="album-cover">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="../../public/images/vowels/a1.gif"
+                alt="Avatar"
+                style="width: 200px; height: 200px"
+              />
+            </div>
+            <div class="flip-card-back">
+              <table class="card">
+                <router-link to="/a1" class="link">
+                  <tr>
+                    <h2>Edie Brickell & New Bohemians</h2>
+                  </tr>
+                  <tr>
+                    <h2>Shooting Rubberbands at the Stars</h2>
+                  </tr>
+                </router-link>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="../../public/images/vowels/a2.gif"
+                alt="Avatar"
+                style="width: 200px; height: 200px"
+              />
+            </div>
+            <div class="flip-card-back">
+              <table>
+                <router-link to="/a12" class="link">
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Coldplay</h2>
+                  </tr>
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X&Y</h2>
+                  </tr>
+                  <br />
+                </router-link>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="../../public/images/vowels/a3.gif"
+                alt="Avatar"
+                style="width: 200px; height: 200px"
+              />
+            </div>
+            <div class="flip-card-back">
+              <table>
+                <router-link to="/a3" class="link">
+                  <tr>
+                    <h2>Wu-Tang Clan</h2>
+                  </tr>
+                  <tr>
+                    <h2>Enter the Wu-Tang (36 Chambers)</h2>
+                  </tr>
+                  <br />
+                </router-link>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="album-cover">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="../../public/images/vowels/a4.gif"
+                alt="Avatar"
+                style="width: 200px; height: 200px"
+              />
+            </div>
+            <div class="flip-card-back">
+              <table class="card">
+                <router-link to="/a4" class="link">
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;Avril Lavigne</h2>
+                  </tr>
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;Love Sux</h2>
+                  </tr>
+                </router-link>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="../../public/images/vowels/a5.gif"
+                alt="Avatar"
+                style="width: 200px; height: 200px"
+              />
+            </div>
+            <div class="flip-card-back">
+              <table>
+                <router-link to="/a5" class="link">
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sonic Youth</h2>
+                  </tr>
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;diRty</h2>
+                  </tr>
+                  <br />
+                </router-link>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="../../public/images/vowels/a6.gif"
+                alt="Avatar"
+                style="width: 200px; height: 200px"
+              />
+            </div>
+            <div class="flip-card-back">
+              <table>
+                <router-link to="/a6" class="link">
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fugees</h2>
+                  </tr>
+                  <tr>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The Score</h2>
+                  </tr>
+                  <br />
+                </router-link>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "HomeView",
+  components: {
+    
+  },
+};
+</script>
+
+<style>
+img {
+  margin: 0 auto;
+}
+body {
+  width: 99%;
+  height: 100%;
+}
+.container {
+  display: flex;
+  margin-bottom: 20px;
+  margin-top: -65px;
+}
+::-webkit-scrollbar {
+  width: 10px;
+}
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey;
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(210 64 255);
+  border-radius: 10px;
+}
+.text {
+  color: #00ffc0;
+  font-family: "Koulen", cursive;
+  font-size: 56px;
+  width: 100%;
+  margin-top: -43px;
+  margin-left: -8px;
+  border-radius: 12px;
+}
+.text2 {
+  color: #94ff00;
+  font-family: "Koulen", cursive;
+  font-size: 40px;
+  width: 100%;
+  margin-top: 18px;
+  margin-left: -385px;
+  border-radius: -8px;
+}
+.text3 {
+    color: #00caff;
+    font-family: "Koulen", cursive;
+    font-size: 30px;
+    width: 100%;
+    margin-top: 62px;
+    margin-left: -495px;
+    border-radius: -8px;
+}
+.album-cover {
+  margin-left: -13px;
+  display: flex;
+}
+.cover {
+  width: 200px;
+  height: 200px;
+}
+.flip-card {
+  background-color: transparent;
+  width: 200px;
+  height: 200px;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.4s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 85%;
+  height: 85%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+.flip-card-back {
+  background-color: #29d0a7;
+  color: white;
+  transform: rotateY(180deg);
+  font-family: "Macondo", cursive;
+  font-size: 12px;
+  padding: 15px;
+  height: 76%;
+  width: 76%;
+  border: 10px solid #6d6dff;
+}
+.link {
+  text-decoration: none;
+  margin-left: 0 auto;
+}
+.link2 {
+  text-decoration: none;
+  margin-left: 13px;
+}
+
+@media only screen and (max-width: 800px) {
+  .album-cover {
+    flex-direction: column;
+    margin-left: 105px;
+  }
+  .container {
+    margin-left: 50px;
+  }
+  .text {
+    margin-top: -35px;
+    font-size: 39px;
+    margin-left: -1px;
+  }
+  .text2 {
+    font-size: 30px;
+  }
+  body {
+    
+    width: 99%;
+    height: 100%;
+  }
+  .top-content {
+    margin: 0 auto;
+    width: 390px;
+  }
+  .top-box[data-v-7ba5bd90] {
+    margin: 0 auto;
+    width: 90%;
+  }
+}
+</style>
